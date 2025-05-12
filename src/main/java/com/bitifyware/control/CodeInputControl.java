03e59b2490630f5d129a69b12382d3c2b78c73a8
antipro <antipro@163.com>
2025-05-12 12:12:48 +0800

    FIX Remote Desktop Copy

--- src/main/java/com/bitifyware/control/CodeInputControl.java
+++ src/main/java/com/bitifyware/control/CodeInputControl.java
@@ -649,6 +649,7 @@ public abstract class CodeInputControl extends Control {
      */
     public void paste() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
+        // Sometimes clipboard have string but hasString() returns false
         if (clipboard.hasString() || clipboard.getString() != null) {
             final String text = clipboard.getString();
             if (text != null) {
