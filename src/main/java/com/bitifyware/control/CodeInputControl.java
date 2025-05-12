a8babde841042e0410aad480fb34652ceabd2851
antipro <antipro@163.com>
2025-05-12 12:10:45 +0800

    TEST

--- src/main/java/com/bitifyware/control/CodeInputControl.java
+++ src/main/java/com/bitifyware/control/CodeInputControl.java
@@ -649,7 +649,7 @@ public abstract class CodeInputControl extends Control {
      */
     public void paste() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
-        if (clipboard.hasString() && clipboard.getString() != null) {
+        if (clipboard.hasString() || clipboard.getString() != null) {
             final String text = clipboard.getString();
             if (text != null) {
                 createNewUndoRecord = true;
